df4883c7fb1aa8de62f306b3c59b9fec25d8388b
Makisuo <minetoaster12@gmail.com>
2025-05-01 17:42:07 +0200

    feat: add stuff

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,14 +1,48 @@
+import { Link, useParams } from "@tanstack/solid-router"
+import { useAuth } from "clerk-solidjs"
+import { For, createMemo } from "solid-js"
+import { useDmChannels } from "~/lib/hooks/data/use-dm-channels"
 import { IconHashtag } from "./icons/hashtag"
 
 export const Sidebar = () => {
+	const params = useParams({ from: "/_app/$serverId" })
+	const serverId = createMemo(() => params().serverId)
+
+	const { channels } = useDmChannels(serverId)
+
+	const { userId } = useAuth()
+
+	const computedChannels = createMemo(() => {
+		return channels()
+			.map((channel) => {
+				const friends = channel.users.filter((user) => user.id !== userId())
+				const isSingleDm = friends.length === 1
+
+				if (friends.length === 0) return null
+
+				return {
+					...channel,
+					isSingleDm,
+					friends,
+				}
+			})
+			.filter((channel) => channel !== null)
+	})
 	return (
 		<ul class="flex flex-col gap-3">
-			<ChannelItem />
-			<ChannelItem />
-			<ChannelItem />
-			<ChannelItem />
-			<ChannelItem />
-			<ChannelItem />
+			<For each={computedChannels()}>
+				{(channel) => (
+					<Link to="/$serverId/chat/$id" params={{ serverId: serverId(), id: channel.id }}>
+						<li class="group/sidebar-item flex items-center gap-2 rounded-md px-2 py-1 hover:bg-muted">
+							<span>Avatar here</span>
+
+							<p class="text-muted-foreground group-hover/sidebar-item:text-foreground">
+								{channel.friends.map((friend) => friend.displayName).join(", ")}
+							</p>
+						</li>
+					</Link>
+				)}
+			</For>
 		</ul>
 	)
 }
@@ -17,11 +51,15 @@ export const SidebarItem = () => {
 	return <li class="flex flex-col gap-3 hover:bg-mu">WOW</li>
 }
 
-export const ChannelItem = () => {
+export interface ChannelItemProps {
+	name: string
+}
+
+export const ChannelItem = (props: ChannelItemProps) => {
 	return (
 		<li class="group/sidebar-item flex items-center gap-2 rounded-md px-2 py-1 hover:bg-muted">
 			<IconHashtag class="size-5 text-muted-foreground" />
-			<p class="text-muted-foreground group-hover/sidebar-item:text-foreground">Test Channel</p>
+			<p class="text-muted-foreground group-hover/sidebar-item:text-foreground">{props.name}</p>
 		</li>
 	)
 }
--- src/lib/hooks/data/use-dm-channels.ts
+++ src/lib/hooks/data/use-dm-channels.ts
@@ -1,16 +1,18 @@
 import { useQuery } from "@rocicorp/zero/solid"
-import { createMemo } from "solid-js"
+import { type Accessor, createMemo } from "solid-js"
 import { useZero } from "~/lib/zero-context"
 
-export const useDmChannels = (serverId: string) => {
+export const useDmChannels = (serverId: Accessor<string>) => {
 	const z = useZero()
 
-	const dmChannelQuery = z.query.serverChannels
-		.related("users")
-		.limit(10)
-		.where((eq) => eq.and(eq.cmp("channelType", "=", "direct"), eq.cmp("serverId", "=", serverId)))
+	const dmChannelQuery = createMemo(() =>
+		z.query.serverChannels
+			.related("users")
+			.limit(10)
+			.where((eq) => eq.and(eq.cmp("channelType", "=", "direct"), eq.cmp("serverId", "=", serverId()))),
+	)
 
-	const [recentDmChannels, status] = useQuery(() => dmChannelQuery)
+	const [recentDmChannels, status] = useQuery(dmChannelQuery)
 
 	const isLoading = createMemo(() => status().type !== "complete")
 
