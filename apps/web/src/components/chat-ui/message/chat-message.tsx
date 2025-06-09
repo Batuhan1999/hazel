4c4a480c6c0ac4b6507644f58332ca8ffd1bd298
Makisuo <minetoaster12@gmail.com>
2025-06-10 01:43:00 +0200

    fix; error boundary

--- apps/web/src/components/chat-ui/message/chat-message.tsx
+++ apps/web/src/components/chat-ui/message/chat-message.tsx
@@ -85,13 +85,7 @@ export function ChatMessage(props: ChatMessageProps) {
 				/>
 
 				<MessageHeader message={props.message} showAvatar={showAvatar} serverId={props.serverId} />
-				<ErrorBoundary fallback={<p>{props.message().content}</p>}>
-					<MessageContent
-						message={props.message}
-						serverId={props.serverId}
-						showAvatar={showAvatar}
-					/>
-				</ErrorBoundary>
+				<MessageContent message={props.message} serverId={props.serverId} showAvatar={showAvatar} />
 			</div>
 		</div>
 	)
