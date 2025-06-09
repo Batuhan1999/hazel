2677b73eb10f6c6ceb78353679d17577681e42fb
Makisuo <minetoaster12@gmail.com>
2025-06-10 01:39:39 +0200

    fix: color

--- apps/web/src/components/chat-ui/message/chat-message.tsx
+++ apps/web/src/components/chat-ui/message/chat-message.tsx
@@ -85,7 +85,7 @@ export function ChatMessage(props: ChatMessageProps) {
 				/>
 
 				<MessageHeader message={props.message} showAvatar={showAvatar} serverId={props.serverId} />
-				<ErrorBoundary fallback={<p class="text-destructive">{props.message().content}</p>}>
+				<ErrorBoundary fallback={<p>{props.message().content}</p>}>
 					<MessageContent
 						message={props.message}
 						serverId={props.serverId}
