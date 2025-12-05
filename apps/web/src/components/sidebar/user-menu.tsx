5b399c259ab240c9bff47707069cc512f0706daa
Makisuo <minetoaster12@gmail.com>
2025-12-06 00:02:09 +0100

    fix: email getting truncated

--- apps/web/src/components/sidebar/user-menu.tsx
+++ apps/web/src/components/sidebar/user-menu.tsx
@@ -1,4 +1,4 @@
-import { ChartPieIcon, ChevronUpDownIcon, ShieldCheckIcon } from "@heroicons/react/20/solid"
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid"
 import { twJoin } from "tailwind-merge"
 import { Avatar } from "~/components/ui/avatar"
 import {
@@ -36,7 +36,7 @@ export function UserMenu() {
 				className="flex w-full items-center justify-between rounded-lg border bg-accent/20 px-2 py-1 hover:bg-accent/50"
 				aria-label="Profile"
 			>
-				<div className="flex items-center gap-x-2">
+				<div className="flex min-w-0 items-center gap-x-2">
 					<Avatar
 						className={twJoin([
 							"[--avatar-radius:7%] group-data-[state=collapsed]:size-6 group-data-[state=collapsed]:*:size-6",
@@ -46,9 +46,9 @@ export function UserMenu() {
 						src={avatarUrl}
 					/>
 
-					<div className="in-data-[collapsible=dock]:hidden text-sm">
+					<div className="in-data-[collapsible=dock]:hidden min-w-0 text-sm">
 						<SidebarLabel>{displayName}</SidebarLabel>
-						{user?.email && <span className="-mt-0.5 block text-muted-fg">{user.email}</span>}
+						{user?.email && <span className="-mt-0.5 block max-w-36 truncate text-muted-fg">{user.email}</span>}
 					</div>
 				</div>
 				<ChevronUpDownIcon data-slot="chevron" className="size-4" />
@@ -60,7 +60,7 @@ export function UserMenu() {
 				<MenuSection>
 					<MenuHeader separator>
 						<span className="block">{displayName}</span>
-						{user?.email && <span className="font-normal text-muted-fg">{user.email}</span>}
+						{user?.email && <span className="block truncate font-normal text-muted-fg">{user.email}</span>}
 					</MenuHeader>
 				</MenuSection>
 
